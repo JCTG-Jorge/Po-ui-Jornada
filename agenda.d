6353f9df259ddc6f542820eb50e931a93db29d8c
JCTG-Jorge <160171828+JCTG-Jorge@users.noreply.github.com>
2024-07-22 22:30:23 -0300

    Add files via upload

---
+++ agenda.d
@@ -0,0 +1,65 @@
+1 "Ana" "29 278869420" ? 0 no
+2 "Antìnio" "38 128451235" ? 0 no
+3 "Bruno" "95 695521583" ? 0 no
+4 "Beatriz" "25 854986459" ? 0 no
+5 "Carlos" "94 543197849" ? 0 no
+6 "Cl†udia" "31 176437098" ? 0 no
+7 "Daniel" "56 613692441" ? 0 no
+8 "Diana" "16 670764734" ? 0 no
+9 "Eduardo" "71 962784210" ? 0 no
+10 "Eliana" "94 601212586" ? 0 no
+11 "Fabio" "21 613882845" ? 0 no
+12 "Fernanda" "36 286892946" ? 0 no
+13 "Gabriel" "23 876578152" ? 0 no
+14 "Giovana" "82 836752948" ? 0 no
+15 "Henrique" "16 326607223" ? 0 no
+16 "Helena" "58 696978253" ? 0 no
+17 "Igor" "46 316441802" ? 0 no
+18 "Isabela" "81 103125769" ? 0 no
+19 "Jo∆o" "76 316563452" ? 0 no
+20 "Juliana" "86 121042222" ? 0 no
+21 "Kleber" "16 333519686" ? 0 no
+22 "Karina" "74 580566400" ? 0 no
+23 "Lucas" "89 371256434" ? 0 no
+24 "L£cia" "65 716464295" ? 0 no
+25 "Marcelo" "45 973219600" ? 0 no
+26 "Mariana" "59 324639597" ? 0 no
+27 "Nelson" "66 918277590" ? 0 no
+28 "Nat†lia" "71 100450583" ? 0 no
+29 "Ot†vio" "77 329849513" ? 0 no
+30 "Ol°via" "46 588271840" ? 0 no
+31 "Paulo" "88 337812653" ? 0 no
+32 "Patr°cia" "82 634070502" ? 0 no
+33 "Quirino" "34 377667996" ? 0 no
+34 "Queila" "58 710806340" ? 0 no
+35 "Rafael" "52 794950878" ? 0 no
+36 "Renata" "48 881351523" ? 0 no
+37 "SÇrgio" "36 725382335" ? 0 no
+38 "Sandra" "46 945695132" ? 0 no
+39 "Thiago" "79 860343483" ? 0 no
+40 "Tatiana" "92 391918653" ? 0 no
+41 "Ubiratan" "22 111691083" ? 0 no
+42 "Èrsula" "92 702488980" ? 0 no
+43 "Vitor" "40 769490767" ? 0 no
+44 "Vanessa" "37 290073018" ? 0 no
+45 "Wagner" "29 707593374" ? 0 no
+46 "Wanessa" "34 726018755" ? 0 no
+47 "Xavier" "95 778422977" ? 0 no
+48 "Xània" "96 248413886" ? 0 no
+49 "Yuri" "65 419637955" ? 0 no
+50 "Yasmin" "69 433926906" ? 0 no
+51 "Zacarias" "23 296915638" ? 0 no
+52 "Zuleica" "30 674550130" ? 0 no
+53 "µgata" "38 128451235" ? 0 no
+.
+PSC
+filename=agenda
+records=0000000000053
+ldbname=mgesp
+timestamp=2024/07/21-11:30:00
+numformat=46,44
+dateformat=dmy-1950
+map=NO-MAP
+cpstream=ibm850
+.
+0000001857
